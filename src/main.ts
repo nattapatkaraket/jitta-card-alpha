7459109a2c60d92eb3ef9be1d1b299cd252e315e
wisdomage.part.ltd@gmail.com <wisd0m@ge3971ptn>
2024-09-06 15:56:51 +0700

    doc: readme and create init material

--- src/main.ts
+++ src/main.ts
@@ -12,17 +12,13 @@ async function bootstrap() {
   app.setGlobalPrefix(`api/${process.env.API_VERSION}`);
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
   const options = new DocumentBuilder()
-    .setTitle('Medical Cart Api Docs')
+    .setTitle('Jitta Card Alpha Api Docs')
     .setVersion(process.env.SWAGGER_VERSION)
     .setBasePath(`/api/${process.env.API_VERSION}`)
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup(
-    `api/${process.env.API_VERSION}/documents`,
-    app,
-    document,
-  );
+  SwaggerModule.setup(`api/${process.env.API_VERSION}/documents`, app, document);
 
   await app.listen(process.env.PORT);
 }
